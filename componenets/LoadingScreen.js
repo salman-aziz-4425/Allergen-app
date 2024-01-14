a3765b35d2e8bd6dc9186b4fc9e1fe37dc335422
Salman4426 <salman.aziz@devsinc.com>
2024-01-14 21:25:24 +0500

    latest changes

--- componenets/LoadingScreen.js
+++ componenets/LoadingScreen.js
@@ -1,15 +1,15 @@
-import {React,useEffect,useMemo} from 'react';
+import { React, useEffect, useMemo } from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 export default function LoadingScreen() {
   return (
-    <SafeAreaView style={{flex:1,display:"flex",alignItems:"center",justifyContent:"center"}}>
-        <Animatable.Image 
-        source={require('C:/Users/Roshaan Aziz/Desktop/PersonalProjects/AwesomeProject/assets/animation-loader.gif')}
+    <SafeAreaView style={{ flex: 1, display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <Animatable.Image
+        source={require('/home/dev/Desktop/Salman_playground/Allergen-app/assets/animation-loader.gif')}
         delay={0}
-        style={{height:50,width:50,backgroundColor:"white"}}
-        >
-        </Animatable.Image>
-     </SafeAreaView>
+        style={{ height: 50, width: 50, backgroundColor: "white" }}
+      >
+      </Animatable.Image>
+    </SafeAreaView>
   );
 }
